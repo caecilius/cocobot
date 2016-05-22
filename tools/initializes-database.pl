5599a8221238048cf64eb7be4fa81aaeb77eb925
caecilius <caecilius@firemail.cc>
2016-05-23 01:08:31 +0200

    Create initializes-database.pl

---
+++ tools/initializes-database.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/perl
+# @created 2012-03-11
+# @date 2012-05-15
+# @author Simon Rubinstein <ssimonrubinstein1@gmail.com>
+# http://code.google.com/p/cocobot/
+#
+# copyright (c) Simon Rubinstein 2010-2012
+# Id: $Id: initializes-database.pl 267 2012-05-15 15:36:15Z ssimonrubinstein1@gmail.com $
+# Revision: $Revision: 267 $
+# Date: $Date: 2012-05-15 15:36:15 +0000 (Tue, 15 May 2012) $
+# Author: $Author: ssimonrubinstein1@gmail.com $
+# HeadURL: $HeadURL: http://cocobot.googlecode.com/svn/trunk/tools/initializes-database.pl $
+#
+# cocobot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# cocobot is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+use strict;
+use warnings;
+use FindBin qw($Script $Bin);
+use Data::Dumper;
+use utf8;
+no utf8;
+use lib "../lib";
+use Cocoweb;
+use Cocoweb::CLI;
+use Cocoweb::DB::Base;
+my $CLI;
+
+init();
+run();
+
+sub run {
+    my $DB = Cocoweb::DB::Base->getInstance();
+    $DB->connect();
+    $DB->dropTables();
+    $DB->createTables();
+    
+    # initializes 'towns' table
+    my ( $town_ref, $towns ) = $DB->getInitTowns();
+    undef $town_ref;
+    $town_ref = $towns->all();
+    foreach my $name (@$town_ref) {
+        $DB->insertTown($name);
+    }
+    undef $town_ref;
+
+    # initializes 'ISPs' table
+    my ( $ISP_ref, $ISPConf ) = $DB->getInitISPs();
+    undef $ISP_ref;
+    $ISP_ref = $ISPConf->all();
+    foreach my $name (@$ISP_ref) {
+        $DB->insertISP($name);
+    }
+    undef $ISP_ref;
+
+    # initializes 'citydios' table
+    my $allZipCodes =
+      Cocoweb::Config->instance()->getConfigFile( 'zip-codes.txt', 'ZipCodes' );
+    $allZipCodes->extract();
+    my $citydio2zip_ref = $allZipCodes->citydio2zip();
+    foreach my $citydio ( keys %$citydio2zip_ref ) {
+        my $townzz = $citydio2zip_ref->{$citydio};
+        $DB->insertCitydio( $citydio, $townzz );
+    }
+
+    info("The $Bin script was completed successfully.");
+}
+
+## @method void init()
+sub init {
+    $CLI = Cocoweb::CLI->instance();
+    my $opt_ref = $CLI->getMinimumOpts();
+    if ( !defined $opt_ref ) {
+        HELP_MESSAGE();
+        exit;
+    }
+}
+
+## @method void HELP_MESSAGE()
+# Display help message
+sub HELP_MESSAGE {
+    print <<ENDTXT;
+Usage: 
+ $Script [-v -d -a myavatar -p mypass]
+  -v          Verbose mode
+  -d          Debug mode
+ENDTXT
+    exit 0;
+}
+
+## @method void VERSION_MESSAGE()
+sub VERSION_MESSAGE {
+    print STDOUT <<ENDTXT;
+    $Script $Cocoweb::VERSION (2012-05-15) 
+     Copyright (C) 2010-2012 Simon Rubinstein 
+     Written by Simon Rubinstein 
+ENDTXT
+}
+
